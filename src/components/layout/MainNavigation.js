45a9e0eb6556de2d72c87be65ff1aef87f6856dd
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-23 16:04:13 +0530

    Layout UI

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import classes from './MainNavigation.module.css'
+export default function MainNavigation(){
+
+    return(
+        <header className={classes.header}>
+            <div className={classes.logo}>Your Quotes</div>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <NavLink to="/quotes" activeClassName={classes.active}>
+                            All Quotes
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/new-quote" activeClassName={classes.active}>
+                            Add Quotes
+                        </NavLink>
+                    </li>
+    
+                </ul>
+            </nav>
+        </header>
+    )
+}
