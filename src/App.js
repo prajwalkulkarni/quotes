20449ec957556a9a759ba3c26214b09bc9610e40
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-25 11:03:30 +0530

    Fix suspense fallback warning

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
+import React,{Suspense} from 'react';
 import {Route,Switch, Redirect} from 'react-router-dom'
-import QuoteList from './components/quotes/QuoteList';
-import Quotes from './pages/Quotes'
-import AddQuote from './pages/AddQuote'
-import NotFound from './pages/NotFound';
-import QuoteDetail from './pages/QuoteDetail'
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const Quotes = React.lazy(()=>import('./pages/Quotes'))
+const AddQuote = React.lazy(()=>import('./pages/AddQuote'))
+const QuoteDetail = React.lazy(()=>import('./pages/QuoteDetail'))
+const NotFound = React.lazy(()=>import('./pages/NotFound'))
 function App() {
   
   return (
     <Layout>
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/quotes'/>
-      </Route>
-      <Route path="/quotes" exact>
-        <Quotes/>
-      </Route>
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
+      <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path="/quotes" exact>
+            <Quotes />
+          </Route>
 
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail/>
-      </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
 
-      <Route path="/new-quote">
-        <AddQuote/>
-      </Route>
+          <Route path="/new-quote">
+            <AddQuote />
+          </Route>
 
-      <Route path="*">
-        <NotFound/>
-      </Route>
-    </Switch>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        
+      </Switch>
+      </Suspense>
+      
     </Layout>
   );
 }
