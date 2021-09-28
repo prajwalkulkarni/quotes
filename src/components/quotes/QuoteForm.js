4428f5eac18d3b58d23a10e681cb2daf0b7dd279
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-28 13:31:13 +0530

    Update color theme

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -9,6 +9,7 @@ import { Fragment } from 'react/cjs/react.production.min';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isValid,setIsValid] = useState(true)
   const [isFocused,setIsFocused] = useState(false)
 
   function submitFormHandler(event) {
@@ -18,6 +19,10 @@ const QuoteForm = (props) => {
     const enteredText = textInputRef.current.value;
 
     // optional: Could validate here
+    if(enteredAuthor.trim()==="" || enteredText.trim() ===""){
+      setIsValid(false)
+      return
+    }
 
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
@@ -26,10 +31,19 @@ const QuoteForm = (props) => {
     setIsFocused(true)
   }
 
+  function addQuoteHandler(){
+    setIsFocused(false)
+  }
+
+  const focusHandler = () =>{
+    setIsValid(true)
+  }
+
   return (
     <Fragment>
       <Prompt when={isFocused} message={(location)=>
       'Are you sure you want to navigate away? All the changes made will be lost'}/>
+      {!isValid && <p className={classes.error}>One or more fields empty!</p>}
     <Card>
       <form onFocus={formFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
@@ -40,14 +54,14 @@ const QuoteForm = (props) => {
 
         <div className={classes.control}>
           <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
+          <input type='text' id='author' onFocus={focusHandler} ref={authorInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          <textarea id='text' rows='5' onFocus={focusHandler} ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button className='btn' onClick={addQuoteHandler}>Add Quote</button>
         </div>
       </form>
     </Card>
