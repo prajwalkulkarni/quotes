41b9c0e8e4d492367676a49258ead2c90fde252d
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-28 13:35:51 +0530

    Update Quotes.js

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -4,24 +4,7 @@ import useHttp from "../hooks/use-http";
 import { getAllQuotes } from "../lib/api";
 import NoQuotesFound from '../components/quotes/NoQuotesFound'
 import LoadingSpinner from '../components/UI/LoadingSpinner'
-export const DUMMY_QUOTES = [
-    {
-        id:'q1',
-        author:'Steve Jobs',
-        text:'The only way to do great things is to love what you do!'
-    },
-    {
-        id:'q2',
-        author:'Mark Zuckerberg',
-        text:'Move fast and break things. Unless you are breaking stuff, you are not moving fast enough.'
 
-    },
-    {
-        id:'q3',
-        author:'Elon Musk',
-        text:'When something is important enough, you do it even if the odds are not in your favor.'
-    }
-]
 export default function Quotes(){
 
     const {sendRequest, status, data:loadedQuotes,error} = useHttp(getAllQuotes,true)
